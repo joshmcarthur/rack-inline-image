10d34d025e4f1c09be12959137e4c44b6f49f1a4
Josh McArthur <joshua.mcarthur@gmail.com>
2011-02-23 21:29:45 +1300

    Added option to skip image embedding with no-encode class on image element

--- lib/inline_image.rb
+++ lib/inline_image.rb
@@ -3,36 +3,41 @@ require 'open-uri'
 require 'base64'
 
 class InlineImage
+  
+  IMAGE_SIZE_MAXIMUM_HEIGHT = 250
+  IMAGE SIZE MAXIMUM_WIDTH = 250
+  
   def initialize(app)
     @app = app
   end
   
   def call(env)
     #Status is an integer (200, 400 etc), headers is a string-string hash, and response is an object (response object from rails)
-    request = Rack::Request.new(env)
+    @request = Rack::Request.new(env)
     status, headers, response = @app.call(env)
     doc = Hpricot(response.body)
-    debugger
     (doc/"img").each do |img|
-      img.attributes['src'] = encodify_image(img.attributes['src'], request.url.match(/\A(http:\/\/|https:\/\/)/)[1] + request.host_with_port)
+      img.attributes['src'] = encodify_image(img.attributes['src'], request.url.match(/\A(http:\/\/|https:\/\/)/)[1] + request.host_with_port) if should_encodify?(img)
     end
     [status, headers, doc.to_html]
   end
   
   private
   def encodify_image(image_location, hostname = "")
-    #begin
+    begin
       image_location = hostname + image_location if hostname
       open(image_location) do |file|
         content_type = File.exists?(image_location) ? image_type(image_location) : file.content_type
         return "data:#{content_type};base64,#{Base64.encode64(file.read)}"
       end
-    #rescue
-    #  return image_location
-   # end
+    rescue
+      return image_location
+    end
   end
   
-  private
+  def should_encodify?(image)
+    return image.attributes['class'] =~ /no-encode/
+  end
   
   def image_type(file)
     case IO.read(file, 10)
